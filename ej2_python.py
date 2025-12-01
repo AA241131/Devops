873b3c8462e6393cfe69817f845bfb54bd292ed0
Andres <AA241131@fi365.ort.edu.uy>
2025-12-01 02:14:10 -0300

    correccion path s3

--- ej2_python.py
+++ ej2_python.py
@@ -1,22 +1,25 @@
 import boto3
 import time
 import json
+from botocore.exceptions import ClientError
+import os
 
 
 #crear el cliente s3
 s3 = boto3.client('s3')
 
 # Parámetros
-bucket_name = 'AA24131_bucket_obligatorio'
-file_path = ['/archivo_archivos_bucket/app.css',
-            '/archivo_archivos_bucket/app.js',
-            '/archivo_archivos_bucket/config.php',
-            '/archivo_archivos_bucket/index.html',
-            '/archivo_archivos_bucket/login.css',
-            '/archivo_archivos_bucket/login.html',
-            '/archivo_archivos_bucket/login.js',
-            '/archivo_archivos_bucket/login.php',
-            '/archivo_archivos_bucket/init_db.sql'
+bucket_name = 'aa24131-bucket-obligatorio'
+path_al_script = os.path.dirname(os.path.abspath(__file__))
+file_path = ['/archivos_bucket/app.css',
+            '/archivos_bucket/app.js',
+            '/archivos_bucket/config.php',
+            '/archivos_bucket/index.html',
+            '/archivos_bucket/login.css',
+            '/archivos_bucket/login.html',
+            '/archivos_bucket/login.js',
+            '/archivos_bucket/login.php',
+            '/archivos_bucket/init_db.sql'
             ]
              
 object_name = []
@@ -24,8 +27,7 @@ object_name = []
 for i in file_path:
     object_name.append(i.split('/')[-1])
 
-
-# Crear un bucket de S3
+Crear un bucket de S3
 try:
     s3.create_bucket(Bucket=bucket_name)
     print(f"Bucket creado: {bucket_name}")
@@ -37,12 +39,12 @@ except ClientError as e:
         exit(1)
 
 #Subir archivos al bucket
-for i in range(len(file_path)-1):
+for i in range(len(file_path)-1):    
     try:
-        s3.upload_file(file_path[i], bucket_name, object_name[i])
-        print(f"Archivo {file_path} subido a {bucket_name}/{object_name}")
+        s3.upload_file(path_al_script+file_path[i], bucket_name, object_name[i])
+        print(f"Archivo {path_al_script+file_path[i]} subido a {bucket_name}/{object_name[i]}")
     except FileNotFoundError:
-        print(f"El archivo {file_path} no existe")
+        print(f"El archivo {file_path[i]} no existe")
     except ClientError as e:
         print(f"Error subiendo archivo: {e}") 
     
